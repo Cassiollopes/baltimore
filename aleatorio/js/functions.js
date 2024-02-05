30da1ad71546dcd1600797d25a3568662032246a
Cassio Eduardo Alves Lopes <cassiollopes11@gmail.com>
2024-02-05 20:25:31 -0300

    a

--- aleatorio/js/functions.js
+++ aleatorio/js/functions.js
@@ -15,3 +15,21 @@ $(function(){
     })
 });
 */
+
+$(function(){
+    $('.fotos').hover(function(){
+        $('.a').css('transform', 'translateX(10%)')
+        .css('transition', '1s');
+        $('.a i').css('scale', 1.2).css('transition', '1s')
+        .css('color', 'white');
+        $('.a p').css('color', 'darkorange').css('transition', '1s');
+        $('.img-1').css('height', '500px').css('transition', '1s');
+    },function(){
+        $('.a').css('transform', 'translateX(0%)')
+        .css('transition', '1s');
+        $('.a i').css('scale', 1).css('transition', '1s')
+        .css('color', 'gray');
+        $('.a p').css('color', 'gray').css('transition', '1s');
+        $('.img-1').css('height', '400px').css('transition', '1s');
+    })
+});
