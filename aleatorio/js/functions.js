8c8f16baa553953921855582fefd89b4c851673a
Cassio Eduardo Alves Lopes <cassiollopes11@gmail.com>
2024-02-05 18:39:35 -0300

    a

---
+++ aleatorio/js/functions.js
@@ -0,0 +1,17 @@
+ /*
+$(function(){
+    setTimeout(function(){
+        $('.img-img').css('opacity', 1).css('transition', '2s')
+    }, 3000)
+
+    $('.container-fts').hover(function(){
+        $('.img-img').css('opacity', 1).css('transition', '1s')
+    },function(){
+        $('.img-img').css('opacity', 0).css('transition', '1s')
+    })
+
+    $(window).scroll(function(){
+        $('.img-img').css('opacity', 1).css('transition', '1s')
+    })
+});
+*/
